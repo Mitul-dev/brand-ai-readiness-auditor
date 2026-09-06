434d0470bb1d2276456ffaabbce7f0c53200a610
Mitul-dev <mitulseth@outlook.com>
2026-09-06 11:57:10 +0530

    Added Deployable Web Dashboard

--- app.py
+++ app.py
@@ -1970,9 +1970,10 @@ def download_html():
 
 
 if __name__ == "__main__":
+    import os
 
     app.run(
-        host="127.0.0.1",
-        port=8000,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000)),
         debug=False,
     )
